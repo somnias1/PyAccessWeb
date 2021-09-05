2de83cc4e972bf831a6ed2938206eec2ddf59d9f
Lizardo <lizardo@utp.edu.co>
2021-09-05 16:09:26 -0500

    Traer un documento XML y extraer a partir de sus propiedades

---
+++ Semana5/XMLParsing.py
@@ -0,0 +1,31 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter location: ')
+if len(url) < 1:
+    url = "http://python-data.dr-chuck.net/comments_200531.xml"
+print('Retrieving', url)
+
+xml = urllib.request.urlopen(url, context=ctx)
+data = xml.read()
+print('Retrieved:', len(data), 'characters')
+
+tree = ET.fromstring(data)
+
+counts = tree.findall('.//count')
+print('Count:', len(counts))
+
+accs = 0
+
+for count in counts:
+    accs += int(count.text)
+
+print('Sum:', accs)
